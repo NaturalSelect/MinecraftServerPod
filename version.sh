36f85b3f0c156289f1c0d930855b19796d4eceee
NaturalSelect <2145973003@qq.com>
2025-03-12 20:45:51 +0800

    chore: use forge
    Signed-off-by: NaturalSelect <2145973003@qq.com>

--- version.sh
+++ version.sh
@@ -7,5 +7,5 @@ fi
 
 if test -z ${URL}
 then
-    URL="https://github.com/IzzelAliz/Arclight/releases/download/FeudalKings%2F1.0.0/arclight-fabric-1.21.1-1.0.0-3457560.jar"
+    URL="https://github.com/IzzelAliz/Arclight/releases/download/FeudalKings%2F1.0.0/arclight-neoforge-1.21.1-1.0.0-3457560.jar"
 fi
